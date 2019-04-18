2e93a354eb991d18e24a3eda5dd924734b3eadc0
dealwithabsolute <klimenix@gmail.com>
2019-04-18 12:08:17 +0300

    first commit

---
+++ src/test/java/com/example/twousers/TwousersApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.twousers;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TwousersApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
